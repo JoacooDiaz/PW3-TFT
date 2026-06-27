71f1ae223acde4a20a6815244b0947864db9b0ae
MateoColace <166334360+MateoColace@users.noreply.github.com>
2026-06-27 01:26:51 -0300

    Actualización visual chico, falta mucho

--- Scripts/UI/MenuPrincipal.cs
+++ Scripts/UI/MenuPrincipal.cs
@@ -3,30 +3,22 @@ using System;
 
 public partial class MenuPrincipal : Control
 {
-	private Button _btnJugar;
-	private Button _btnOpciones;
-    private Button _btnSalir;
+    private TextureButton _btnJugar;
+    private TextureButton _btnSalir;
 
     public override void _Ready()
     {
-        _btnJugar = GetNode<Button>("VBoxContainer/BtnJugar");
-        _btnOpciones = GetNode<Button>("VBoxContainer/BtnOpciones");
-        _btnSalir = GetNode<Button>("VBoxContainer/BtnSalir");
+        _btnJugar = GetNode<TextureButton>("VBoxContainer/BtnJugar");
+        _btnSalir = GetNode<TextureButton>("VBoxContainer/BtnSalir");
 
         _btnJugar.Pressed += OnBtnJugarPressed;
-        _btnOpciones.Pressed += OnBtnOpcionesPressed;
         _btnSalir.Pressed += OnBtnSalirPressed;
+        GD.Print("SOY EL SCRIPT NUEVO");
     }
 
     private void OnBtnJugarPressed()
     {
-        GD.Print("Comenzaste el Juego");
-        GetTree().ChangeSceneToFile("uid://co5gt2hmtwe4i");
-    }
-
-    private void OnBtnOpcionesPressed()
-    {
-        GetTree().ChangeSceneToFile("uid://c4co0np2apqqm");
+        GD.Print("Iniciaste la partida");
     }
 
     private void OnBtnSalirPressed()
